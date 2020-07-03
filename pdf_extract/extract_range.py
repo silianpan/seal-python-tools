1ca550011a9725854dcccef3dce91ed502a5868d
twofloor <1206284818@qq.com>
2020-07-03 11:46:25 +0800

    pdf按照范围批量提取页面

--- pdf_extract/extract_range.py
+++ pdf_extract/extract_range.py
@@ -10,11 +10,12 @@ import os
 from PyPDF2 import PdfFileReader, PdfFileWriter
 
 
-def extract_range(file_name, full_name, split_range, output_dir):
+def extract_range(full_name, split_range, output_dir):
     # 转化为PDF文件对象
     pdfFileObj = PdfFileReader(full_name)
 
-    split_pages = split_range.split(',')
+    split_pages = split_range.page.split(',')
+    i = 0
     for split_page in split_pages:
         page_range = split_page.split('-')
         page_start = int(page_range[0])
@@ -34,7 +35,8 @@ def extract_range(file_name, full_name, split_range, output_dir):
             pdfWriter.addPage(pageObj)
 
         # Extracted pdf file path
-        pdf_file_path = os.path.join(output_dir, file_name + '-页面-{}-{}.pdf'.format(page_start, page_end))
+        pdf_file_path = os.path.join(output_dir, split_range.text[i] + '-{}-{}.pdf'.format(page_start, page_end))
+        i = i + 1
 
         with open(pdf_file_path, 'wb') as pdfOutputFile:
             pdfWriter.write(pdfOutputFile)
@@ -42,17 +44,28 @@ def extract_range(file_name, full_name, split_range, output_dir):
 
 def start():
     split_range_list = [
-        '2-20,21-23,24-26,27-32,33-38,39-42,43-49'
+        {
+            'page': '2-20,21-23,24-26,27-32,33-38,39-42,43-49',
+            'text': [
+                '2018年政府工作报告',
+                '西藏自治区人民政府关于废止和宣布失效部分规范性文件的决定',
+                '西藏自治区人民政府办公厅关于严格控制新设行政许可的通知',
+                '西藏自治区人民政府办公厅印发关于推进农业水价综合改革实施方案的通知',
+                '西藏自治区人民政府办公厅关于印发西藏自治区推进“互联网+政务服务”实施方案的通知',
+                '西藏自治区人民政府办公厅关于印发推进全区城镇人口密集区危险化学品生产企业搬迁改造工作实施方案的通知',
+                '政府定价经营服务性收费目录清单'
+            ]
+        }
     ]
 
-    pdf_dir = '/Users/panliu/Downloads/自治区政府公报'
-    output_dir = '/Users/panliu/Downloads/自治区政府公报提取页面输出'
+    pdf_dir = './input'
+    output_dir = './output'
     if os.path.isdir(pdf_dir):
         i = 0
         for s in os.listdir(pdf_dir):
-            file_name = s[:-4]
+            # file_name = s[:-4]
             full_name = os.path.join(pdf_dir, s)
-            extract_range(file_name, full_name, split_range_list[i], output_dir)
+            extract_range(full_name, split_range_list[i], output_dir)
             i = i + 1
 
 
