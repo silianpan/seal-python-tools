f4088a38206eaca7a69aa3d6f36ab1b3bd31810a
twofloor <1206284818@qq.com>
2020-07-03 16:12:08 +0800

    抽离test1

--- pdf_extract/extract_range.py
+++ pdf_extract/extract_range.py
@@ -11,6 +11,7 @@ from PyPDF2 import PdfFileReader, PdfFileWriter
 
 
 def extract_range(file_name, full_name, split_range, output_dir):
+
     # 转化为PDF文件对象
     pdfFileObj = PdfFileReader(full_name)
 
@@ -40,34 +41,3 @@ def extract_range(file_name, full_name, split_range, output_dir):
 
         with open(pdf_file_path, 'wb') as pdfOutputFile:
             pdfWriter.write(pdfOutputFile)
-
-
-def start():
-    split_range_list = [
-        {
-            'page': '2-20,21-23,24-26,27-32,33-38,39-42,43-49',
-            'text': [
-                '2018年政府工作报告',
-                '西藏自治区人民政府关于废止和宣布失效部分规范性文件的决定',
-                '西藏自治区人民政府办公厅关于严格控制新设行政许可的通知',
-                '西藏自治区人民政府办公厅印发关于推进农业水价综合改革实施方案的通知',
-                '西藏自治区人民政府办公厅关于印发西藏自治区推进“互联网+政务服务”实施方案的通知',
-                '西藏自治区人民政府办公厅关于印发推进全区城镇人口密集区危险化学品生产企业搬迁改造工作实施方案的通知',
-                '政府定价经营服务性收费目录清单'
-            ]
-        }
-    ]
-
-    pdf_dir = './input'
-    output_dir = './output'
-    if os.path.isdir(pdf_dir):
-        i = 0
-        for s in os.listdir(pdf_dir):
-            file_name = s[:-4]
-            full_name = os.path.join(pdf_dir, s)
-            extract_range(file_name, full_name, split_range_list[i], output_dir)
-            i = i + 1
-
-
-if __name__ == '__main__':
-    start()
