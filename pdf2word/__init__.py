6d5a4503efce86bf7953a416653a005e416c266d
liupan <1206284818@qq.com>
2020-10-21 11:32:53 +0800

    pdf2word

---
+++ pdf2word/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/10/21 10:03 上午
+# @Author  : silianpan
+# @Site    : 多线程pdf转word
+# @File    : __init__.py.py
+# @Software: PyCharm
+
+# pdf2docx：https://github.com/silianpan/pdf2docx
+# 安装：pip install pdf2docx
+# 使用：pdf2docx pdf docx --multi_processing=True
