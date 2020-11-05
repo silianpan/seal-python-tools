1c6e4d89bfefc151b32b90587ae28c9e40284a5e
liupan <1206284818@qq.com>
2020-11-05 19:27:27 +0800

    fix

--- pyspider/bio_med_sina.py
+++ pyspider/bio_med_sina.py
@@ -1,67 +1,51 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time    : 2020/11/5 6:04 下午
-# @Author  : silianpan
-# @Site    : 
-# @File    : bio_med_sina.py
-# @Software: PyCharm
-
 import json
 import re
-
 import pymysql
 from fake_useragent import UserAgent
 from pyspider.libs.base_handler import *
-
 pattern_article = re.compile(u'^https://med.sina.com/article_detail_.+.html$')
-
-urls = [
-    'https://med.sina.com/column/zonghe/',
-    'https://med.sina.com/feature_322.html',
-    'https://med.sina.com/feature_284.html',
-    'https://med.sina.com/feature_1341.html'
-]
+urls = ['https://med.sina.com/column/zonghe/','https://med.sina.com/feature_322.html','https://med.sina.com/feature_284.html','https://med.sina.com/feature_1341.html']
 
 class Handler(BaseHandler):
     crawl_config = {}
-
     def __init__(self):
-        pass
-
+        self.conn = pymysql.connect(host='172.16.95.1', user='root', password='Asdf@123', port=3306, db='db-biotown-gdss')
+        self.cursor = self.conn.cursor()
     def __del__(self):
-        pass
-
+        self.conn.close()
     def get_taskid(self, task):
         return md5string(task['url']) + json.dumps(task['fetch'].get('data', ''))
-
     @every(minutes=24 * 60)
     def on_start(self):
-        self.crawl('https://med.sina.com/column/zonghe/', method='GET', callback=self.index_page, user_agent=UserAgent().random)
+        for url in urls:
+            self.crawl(url, validate_cert=False, method='GET', callback=self.index_page, user_agent=UserAgent().random)
 
     @config(age=5 * 24 * 60 * 60)
     def index_page(self, response):
-        next_page = response.doc('a.clickmore')
+        next_page = response.doc('.show > .clickmore_f')
+        if next_page is None:
+            next_page = response.doc('.show > .clickmore_f')
         if next_page is not None:
             sign = next_page.attr('sign')
             if sign is not None:
+                next_url_prefix1 = 'https://med.sina.com/article_list_'
+                next_url_prefix2 = 'https://med.sina.com/feature_list_'
+                next_url = ''
                 signs = sign.strip().split('_')
-                print(signs[0], signs[1], signs[2], signs[3])
-                print(int(signs[2]) + 1)
-                next_sign = int(signs[2]) + 1
-                next_url_prefix = 'https://med.sina.com/article_list_'
-                next_url = next_url_prefix + signs[0] + '_' + signs[1] + '_' + next_sign + '_' + signs[3] + '.html'
-                print(next_url)
-                self.crawl(next_url, method='GET', callback=self.index_page, user_agent=UserAgent().random)
-        # 逐条提取
+                if len(signs) == 4:
+                    next_sign = int(signs[2]) + 1
+                    next_url = next_url_prefix1 + signs[0] + '_' + signs[1] + '_' + str(next_sign) + '_' + signs[3] + '.html'
+                else:
+                    next_sign = int(signs[1]) + 1
+                    next_url = next_url_prefix2 + signs[0] + '_' + str(next_sign) + '_' + signs[2] + '.html'
+                self.crawl(next_url, validate_cert=False, method='GET', callback=self.index_page,
+                           user_agent=UserAgent().random)
         self.item_page(response)
-
     def item_page(self, response):
         ua = UserAgent()
         for each in response.doc('a[href^="http"]').items():
             if re.match(pattern_article, each.attr.href):
-                # title = each.text().strip()
-                self.crawl(each.attr.href, callback=self.detail_page, user_agent=ua.random)
-
+                self.crawl(each.attr.href, validate_cert=False, callback=self.detail_page, user_agent=ua.random)
     @config(priority=2)
     def detail_page(self, response):
         title = response.doc('.news > h1.news-title').text().strip()
@@ -77,8 +61,6 @@ class Handler(BaseHandler):
         }
         self.save_to_mysql(ret)
         return ret
-
-    # 保存数据库
     def save_to_mysql(self, ret):
         insert_sql = """
         INSERT INTO spider_med_sina(url, title, content, pub_date, source) values(%s, %s, %s, %s, %s)
