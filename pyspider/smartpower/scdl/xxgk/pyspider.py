40ae3c37b73e4bd35df4a79571046c6ab5ff6cdc
silianpan <liu.pan@silianpan.cn>
2024-06-21 20:14:29 +0800

    四川电力信息公开-无代理

--- pyspider/smartpower/scdl/xxgk/pyspider.py
+++ pyspider/smartpower/scdl/xxgk/pyspider.py
@@ -25,27 +25,11 @@ class Handler(BaseHandler):
         'connect_timeout': 600,
         # 'timeout': 2,
         'headers': {
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-            'Accept-Encoding': 'gzip, deflate, br, zstd',
-            'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,ja;q=0.7,zh-TW;q=0.6',
-            'Cache-Control': 'no-cache',
-            'Connection': 'keep-alive',
-            'Host': 'www.sc.sgcc.com.cn',
-            'Pragma': 'no-cache',
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
-            'Sec-Ch-Ua': '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
-            'Sec-Ch-Ua-Mobile': '?0',
-            'Sec-Ch-Ua-Platform': '"macOS"',
-            'Sec-Fetch-Dest': 'document',
-            'Sec-Fetch-Mode': 'navigate',
-            'Sec-Fetch-Site': 'none',
-            'Sec-Fetch-User': '?1',
-            'Upgrade-Insecure-Requests': '1',
         }
     }
 
     def __init__(self):
-        self.proxy_list = self.get_proxy_list()
         self.spider_config = {
             "source_url": 'https://www.sc.sgcc.com.cn/html/main',
             'urls': [
@@ -90,7 +74,7 @@ class Handler(BaseHandler):
     def on_start(self):
        for item in self.spider_config['urls']:
            self.crawl(self.spider_config['source_url'] + item['url'], save={'classify': item['classify'], 'classify_name': item['classify_name']},
-                      validate_cert=False, method='GET', callback=self.next_page, proxy='http://{proxy}'.format(proxy=self.get_random_proxy()),
+                      validate_cert=False, method='GET', callback=self.next_page,
                       user_agent=UserAgent().random)
 
     @config(age=10 * 24 * 60 * 60)
@@ -101,15 +85,10 @@ class Handler(BaseHandler):
         for i in range(2, int(pageCount) + 1):
             self.crawl(response.url.replace('1.html', str(i) + '.html'),
                        save={'classify': response.save['classify'], 'classify_name': response.save['classify_name']},
-                       validate_cert=False, method='GET', callback=self.item_page, proxy='http://{proxy}'.format(proxy=self.get_random_proxy()),
+                       validate_cert=False, method='GET', callback=self.item_page,
                        user_agent=UserAgent().random)
 
     def item_page(self, response):
-        if response.status_code in [401, 403, 599]:
-            self.crawl(response.url, save={'classify': response.save['classify'], 'classify_name': response.save['classify_name']},
-                       validate_cer=False, method='GET', callback=self.item_page, proxy='http://{proxy}'.format(proxy=self.update_proxy()),
-                       user_agent=UserAgent().random)
-
         boxs = response.doc('ul.list > li').items()
         for box in boxs:
             art_href = box('a').attr('href')
@@ -127,16 +106,11 @@ class Handler(BaseHandler):
                    self.save_file_data(file_name, file_path, art_href, save)
                 else:
                     self.crawl(art_href, validate_cert=False,
-                               save=save, callback=self.detail_page, proxy='http://{proxy}'.format(proxy=self.get_random_proxy()),
+                               save=save, callback=self.detail_page,
                                      user_agent=UserAgent().random)
 
     @config(priority=2)
     def detail_page(self, response):
-        if response.status_code in [401, 403, 599]:
-            self.crawl(response.url, save={'classify': response.save['classify'], 'classify_name': response.save['classify_name'],
-                                           'title': response.save['title'], 'pub_date': response.save['pub_date']},
-                       validate_cer=False, method='GET', callback=self.item_page, proxy='http://{proxy}'.format(proxy=self.update_proxy()),
-                       user_agent=UserAgent().random)
         content = response.doc('.txtcon').html().strip()
         # b64encode函数的参数为byte类型，所以必须先转码
         contentBytesString = content.encode('utf-8')
@@ -179,6 +153,7 @@ class Handler(BaseHandler):
             'classify_name': save['classify_name'],
             'title': save['title'],
             'pub_date': save['pub_date'],
+            'content': content,
             'json': json.dumps(obj, ensure_ascii=False)
         }
         self.save_to_mysql(self.spider_config['db']['table_name'], ret)
@@ -223,32 +198,32 @@ class Handler(BaseHandler):
             # 发生错误时回滚
             self.conn.rollback()
 
-    def get_proxy_list(self):
-        return requests.get('http://webapi.http.zhimacangku.com/getip?neek=9e2a270f&num=10&type=2&time=4&pro=0&city=0&yys=0&port=1&pack=0&ts=1&ys=0&cs=0&lb=1&sb=&pb=4&mr=3&regions=&cf=0').json()
+    # def get_proxy_list(self):
+    #     return requests.get('http://webapi.http.zhimacangku.com/getip?neek=9e2a270f&num=10&type=2&time=4&pro=0&city=0&yys=0&port=1&pack=0&ts=1&ys=0&cs=0&lb=1&sb=&pb=4&mr=3&regions=&cf=0').json()
     
-    def get_random_proxy(self):
-        if self.proxy_list and self.proxy_list.get('code') == 0:
-            proxy_items = self.proxy_list.get('data')
-            for item in proxy_items:
-                if not self.is_expired(item.get('expire_time')):
-                    return item.get('ip') + ':' + str(item.get('port'))
-        self.proxy_list = self.get_proxy_list()
-        return self.get_random_proxy()
+    # def get_random_proxy(self):
+    #     if self.proxy_list and self.proxy_list.get('code') == 0:
+    #         proxy_items = self.proxy_list.get('data')
+    #         for item in proxy_items:
+    #             if not self.is_expired(item.get('expire_time')):
+    #                 return item.get('ip') + ':' + str(item.get('port'))
+    #     self.proxy_list = self.get_proxy_list()
+    #     return self.get_random_proxy()
     
-    def is_expired(self, dt):
-        """
-        判断时间是否过期
-        :param dt:日期字符串
-        :return:True or False
-        """
-        if isinstance(dt, str):
-            dt = datetime.strptime(dt, '%Y-%m-%d %H:%M:%S')
-            return datetime.now() > dt
-    def update_proxy(self):
-        """
-        重新通过api获取代理，更新代理
-        """
-        # 重新通过api获取代理
-        self.proxy_list = self.get_proxy_list()
-        new_proxy = self.get_random_proxy()
-        return new_proxy
+    # def is_expired(self, dt):
+    #     """
+    #     判断时间是否过期
+    #     :param dt:日期字符串
+    #     :return:True or False
+    #     """
+    #     if isinstance(dt, str):
+    #         dt = datetime.strptime(dt, '%Y-%m-%d %H:%M:%S')
+    #         return datetime.now() > dt
+    # def update_proxy(self):
+    #     """
+    #     重新通过api获取代理，更新代理
+    #     """
+    #     # 重新通过api获取代理
+    #     self.proxy_list = self.get_proxy_list()
+    #     new_proxy = self.get_random_proxy()
+    #     return new_proxy
